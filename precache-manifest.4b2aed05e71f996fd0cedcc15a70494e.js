405de09eb2c0dc54011dcc4eee424e42a36c1684
C2Store <>
2023-06-26 19:42:20 -0700

    Updates

---
+++ precache-manifest.4b2aed05e71f996fd0cedcc15a70494e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de816a525fa175de3b3c189d507dedf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "909af31da825fb976ccf",
+    "url": "/static/css/2.bb57abf7.chunk.css"
+  },
+  {
+    "revision": "399929c64f1644739648",
+    "url": "/static/css/main.bb22aab4.chunk.css"
+  },
+  {
+    "revision": "909af31da825fb976ccf",
+    "url": "/static/js/2.2e2fd0eb.chunk.js"
+  },
+  {
+    "revision": "399929c64f1644739648",
+    "url": "/static/js/main.405e950c.chunk.js"
+  },
+  {
+    "revision": "5ea495261c733488b555",
+    "url": "/static/js/runtime~main.525fd2f5.js"
+  }
+]);
